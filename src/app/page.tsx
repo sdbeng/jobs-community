9559e4eea53330d253cfaafbc8df4c38e6df7c2b
Sergio D. Barrera <sdbeng@gmail.com>
2024-08-13 17:14:39 -0700

    update header, add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      HOME
-      <Button>Click me</Button>
+      <h2>Jobs Community home</h2>
+      
     </div>
   );
 }
