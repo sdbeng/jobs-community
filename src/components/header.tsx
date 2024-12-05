65e61d4e5239ff0e4147f3d3604b7cea3017671b
Sergio Barrera <sdbeng@gmail.com>
2024-12-04 16:00:31 -0800

    refactor header component for improved layout and styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,43 +10,40 @@ const Header = async () => {
     const user = await checkUser();
         
   return (
-    <div className='flex'>
+    <div className='flex items-center justify-between p-4 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500'>
         {/* Image logo */}
-        <Image src='/jc_logo.png' alt='jobs communitylogo' width={40} height={40} className='rounded-lg '/>
-        <div className='flex-1 '>
-            {/* search form */}
-            <form className='flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96 '>
-                <SearchIcon className='h-4 text-gray-600 '/>
-                <input type='text' placeholder='Search for jobs' className='bg-transparent outline-none flex-1'/>
+        <div className='flex items-center space-x-4'>
+            <Image src='/space_x.png' alt='jobs community logo' width={72} height={72} className='rounded-full' />
+            <form className='flex items-center space-x-2 bg-gray-100 p-2 rounded-md'>
+                <SearchIcon className='h-5 text-gray-600' />
+                <input type='text' placeholder='Search for jobs' className='bg-transparent outline-none flex-1' />
             </form>
         </div>
-        <div className='flex items-center space-x-4 px-6'>
+        <div className='flex items-center space-x-6'>
             {/* icons w/ Links */}
-            <Link href='/' className='icon'>
-            <HomeIcon className='h-6 text-gray-600 cursor-pointer'/>
-            <p>Home</p>
+            <Link href='/' className='flex flex-col items-center'>
+                <HomeIcon className='h-6 text-gray-600 cursor-pointer' />
+                <p className='text-sm'>Home</p>
             </Link>
-            <Link href='/' className='icon hidden md:flex '>
-            <UsersIcon className='h-6 text-gray-600 cursor-pointer'/>
-            <p>Network</p>
+            <Link href='/network' className='flex flex-col items-center hidden md:flex'>
+                <UsersIcon className='h-6 text-gray-600 cursor-pointer' />
+                <p className='text-sm'>Network</p>
             </Link>
-            <Link href='/' className='icon hidden md:flex'>
-            <Briefcase className='h-6 text-gray-600 cursor-pointer'/>
-            <p>Jobs</p>
+            <Link href='/jobs' className='flex flex-col items-center hidden md:flex'>
+                <Briefcase className='h-6 text-gray-600 cursor-pointer' />
+                <p className='text-sm'>Jobs</p>
             </Link>
-            <Link href="/chat" className="icon">
-            <MessagesSquare className="h-5" />
-            <p>Public Services</p>
+            <Link href='/chat' className='flex flex-col items-center'>
+                <MessagesSquare className='h-6 text-gray-600 cursor-pointer' />
+                <p className='text-sm'>Public Services</p>
             </Link>
-
             <SignedIn>
-            <UserButton />
+                <UserButton />
             </SignedIn>
-
             <SignedOut>
-            <Button asChild >
-                <SignInButton />
-            </Button>
+                <Button asChild>
+                    <SignInButton />
+                </Button>
             </SignedOut>
         </div>
     </div>
